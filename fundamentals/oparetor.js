fbcff6519483390e118141504b5700c9f911048a
jakdev047 <jakfreelance047@gmail.com>
2021-07-06 10:03:04 +0600

    logical operators & truthy & falsy values

--- fundamentals/oparetor.js
+++ fundamentals/oparetor.js
@@ -30,3 +30,37 @@ num1 == 30; // true
 num1 === "30"; // false
 num1 != 30; // false
 num1 !== 30; // false
+
+// Logical Operator && || !=
+true && true; // true
+true && false; // false
+true || true; // true
+true || false; // true
+true != true; // false
+true !== true; // false
+
+// developer console
+// console.log('%cZonayed Ahmed', 'font-size: 50px; background-color: red')
+
+// Excercise 1
+x;
+const y = "a";
+const p = 3;
+const q = 8;
+
+q === "b" || p >= 10; // false
+!(p === "3" || p === q) && !(q != 8 && p <= q); // true
+
+// Excercise 2
+!"Hello"; // false
+!""; // true
+!null; // true
+!0; // true
+!-1; // false
+!NaN; // true
+
+// Excercise 3
+const str = "";
+const msg = "haha!";
+const isFunny = "false";
+!(str || msg) && isFunny; // false
