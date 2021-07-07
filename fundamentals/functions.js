0f9dd2dad1e494f9e68337210586d119eddfbd45
jakdev047 <jakfreelance047@gmail.com>
2021-07-07 10:50:38 +0600

    function excercise add

--- fundamentals/functions.js
+++ fundamentals/functions.js
@@ -10,10 +10,25 @@ function myFuncState() {
 }
 myFuncState();
 
-// Excersise
+// Excersise 01
 
 const bioSelf = (fullName, profession, language) => {
   return `I am ${fullName}.I am a ${profession}.I Love ${language}.`;
 };
 const jubayerSelf = bioSelf("Jubayer", "Frontend Developer", "JavaScript");
 console.log(jubayerSelf);
+
+// Excercise
+function abTest(a, b) {
+  // Only change code below this line
+
+  if (a < 0 || b < 0) {
+    return undefined;
+  } else {
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  }
+
+  // Only change code above this line
+}
+
+abTest(-2, 2); // undefined
