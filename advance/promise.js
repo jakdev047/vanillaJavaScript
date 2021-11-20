c01d585d683d69aae34902aa39f277a53eaabf07
jakdev047 <jakfreelancer047@gmail.com>
2021-11-20 12:21:28 +0600

    promise

--- advance/promise.js
+++ advance/promise.js
@@ -17,24 +17,79 @@ makeServerRequest
     console.log(error); // Data not received
   });
 
-
 function run() {
-  // promise define 
-  const promise = new Promise(resolve=>{
-    resolve('Promise Resolve');
-  })
+  // promise define
+  const promise = new Promise((resolve) => {
+    resolve("Promise Resolve");
+  });
 
   setTimeout(() => {
-    console.log("Set Time Out")
+    console.log("Set Time Out");
   });
 
   // promise call
-  promise.then((res)=> {
-    console.log(res)
+  promise.then((res) => {
+    console.log(res);
   });
 
-  console.log("Log")
+  console.log("Log");
 }
 
-
 run();
+
+// Excercise 01
+const paymentSuccss = true;
+const marks = 90;
+
+// enrollment function
+function enroll() {
+  console.log("Course Enrollment is in progress...");
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (paymentSuccss) {
+        resolve();
+      } else {
+        reject("Payment failed");
+      }
+    }, 2000);
+  });
+
+  return promise;
+}
+
+// progress function
+function progress() {
+  console.log("Course on progress...");
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (marks >= 80) {
+        resolve();
+      } else {
+        reject("You could not enough marks to get the certificate.");
+      }
+    }, 3000);
+  });
+
+  return promise;
+}
+
+// getCertificate function
+function getCertificate() {
+  console.log("Preparing your certificate!");
+
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Congrats! You got your certificate")
+    }, 1000);
+  });
+
+  return promise;
+}
+
+enroll()
+    .then(progress)
+    .then(getCertificate)
+    .then(value=> console.log(value))
+    .catch(err => console.log(err))
