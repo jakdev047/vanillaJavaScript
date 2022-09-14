bdbc90ab4352df8184e23ee1a7e032bd7b699bc7
jakdev047 <jakfreelance047@gmail.com>
2022-09-14 11:30:48 +0600

    decimal number check

--- functions/decimalNumberSplit.js
+++ functions/decimalNumberSplit.js
@@ -8,3 +8,24 @@ function decimalNumberSplit(number) {
 }
 
 console.log(decimalNumberSplit(12.34));
+
+/*
+  1. the squares of only the positive integers
+  2. decimal numbers are not integers
+
+*/
+const isInteger = (num) => {
+  let result = num - Math.floor(num) === 0;
+  if (result) {
+    return num;
+  } else {
+    return;
+  }
+};
+const squareList = (arr) => {
+  let positiveNumberArr = arr.filter((itm) => itm >= 0 && isInteger(itm));
+  return positiveNumberArr.map((itm) => itm * itm);
+};
+
+const squaredIntegers = squareList([-3.7, -5, 3, 10, 12.5, 7, -4.5, -17, 0.3]);
+console.log(squaredIntegers);
